d24e56776befd5b12b8de4ca5e55ca6a00dafccc
wesaraw <dudewithamask3@gmail.com>
2025-07-06 19:37:43 -0400

    fix: avoid double-counting meal plan consumption

--- popup.js
+++ popup.js
@@ -388,11 +388,17 @@ async function init() {
   const sortedNeeds = sortItemsByCategory(needs);
   const consMap = new Map(consumption.map(c => [c.name, c]));
   const hasCalendar = calendar && Object.keys(calendar).length > 0;
-  (mealMonth || []).forEach(m => {
-    const rec = consMap.get(m.name);
-    if (rec) rec.monthly_consumption += m.monthly_consumption;
-    else consMap.set(m.name, { name: m.name, monthly_consumption: m.monthly_consumption });
-  });
+  if (!hasCalendar) {
+    (mealMonth || []).forEach(m => {
+      const rec = consMap.get(m.name);
+      if (rec) rec.monthly_consumption += m.monthly_consumption;
+      else
+        consMap.set(m.name, {
+          name: m.name,
+          monthly_consumption: m.monthly_consumption
+        });
+    });
+  }
   consumptionData = Array.from(consMap.values());
   consumptionMap = consMap;
   expirationData = expiration;
@@ -557,11 +563,17 @@ async function rerenderAll() {
   const sortedNeeds = sortItemsByCategory(needs);
   const consMap = new Map(consumption.map(c => [c.name, c]));
   const hasCalendar = calendar && Object.keys(calendar).length > 0;
-  (mealMonth || []).forEach(m => {
-    const rec = consMap.get(m.name);
-    if (rec) rec.monthly_consumption += m.monthly_consumption;
-    else consMap.set(m.name, { name: m.name, monthly_consumption: m.monthly_consumption });
-  });
+  if (!hasCalendar) {
+    (mealMonth || []).forEach(m => {
+      const rec = consMap.get(m.name);
+      if (rec) rec.monthly_consumption += m.monthly_consumption;
+      else
+        consMap.set(m.name, {
+          name: m.name,
+          monthly_consumption: m.monthly_consumption
+        });
+    });
+  }
   consumptionData = Array.from(consMap.values());
   consumptionMap = consMap;
   expirationData = expiration;
