1c83319e9e67715098d67ceb21c948648f86b7c7
wesaraw <dudewithamask3@gmail.com>
2025-07-22 12:42:04 -0400

    Fix unit parsing order in scrapers

--- utils/scrapers/shaws.js
+++ utils/scrapers/shaws.js
@@ -106,16 +106,6 @@ export function scrapeShaws() {
     sizeUnit = UNIT_ALIASES[key] || key;
   }
 
-    let totalSizeQty = null;
-    if (sizeQty != null) {
-      totalSizeQty = sizeQty * packCount;
-    } else if (unitQty != null && unitType) {
-      totalSizeQty = unitQty * packCount;
-      sizeUnit = unitType;
-    }
-    sizeQty = totalSizeQty;
-
-    let convertedQty = null;
     const parsedInfo = parseUnitPrice(unitText);
     let pricePerUnit = parsedInfo ? parsedInfo.pricePerUnit : null;
     let unitQty = parsedInfo ? parsedInfo.unitQty : null;
@@ -127,6 +117,17 @@ export function scrapeShaws() {
         unitType = 'oz';
       }
     }
+
+    let totalSizeQty = null;
+    if (sizeQty != null) {
+      totalSizeQty = sizeQty * packCount;
+    } else if (unitQty != null && unitType) {
+      totalSizeQty = unitQty * packCount;
+      sizeUnit = unitType;
+    }
+    sizeQty = totalSizeQty;
+
+    let convertedQty = null;
 
     if (sizeQty != null && sizeUnit) {
       const unit = sizeUnit.toLowerCase();
