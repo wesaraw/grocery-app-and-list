fd4d757b9ed174011c264ea7609676829b4ccbf9
wesaraw <dudewithamask3@gmail.com>
2025-08-04 09:00:48 -0400

    Revert "fix meal id conversion"

--- scripts/convertBackup.js
+++ scripts/convertBackup.js
@@ -40,19 +40,7 @@ function replaceNames(obj, maps) {
   if (obj && typeof obj === 'object') {
     const out = {};
     for (const [k, v] of Object.entries(obj)) {
-      if (k === 'name' && typeof v === 'string') {
-        out[k] = obj.ingredients
-          ? maps.meals[v] || v
-          : maps.items[v] || v;
-      } else if (k === 'item' && typeof v === 'string') {
-        out[k] = maps.items[v] || v;
-      } else if (k === 'store' && typeof v === 'string') {
-        out[k] = maps.stores[v] || v;
-      } else if (k === 'recipeBook' && typeof v === 'string') {
-        out[k] = maps.books[v] || v;
-      } else {
-        out[k] = replaceNames(v, maps);
-      }
+      out[k] = replaceNames(v, maps);
     }
     return out;
   }
