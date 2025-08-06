6fbf2e7ecf0f7273426382ad4a042393a9f060b5
wesaraw <dudewithamask3@gmail.com>
2025-08-06 08:21:03 -0400

    fix: use central item registry in background module

--- background.js
+++ background.js
@@ -1,18 +1,24 @@
+import { getItemId } from './utils/itemRegistry.js';
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'openStoreTab') {
     const { url, item, store } = message;
     (async () => {
-      const { getItemId } = await import(chrome.runtime.getURL('utils/itemRegistry.js'));
       const itemId = await getItemId(item);
       chrome.tabs.create({ url }, tab => {
-        chrome.storage.local.set({ currentItemInfo: { itemId, store, tabId: tab.id } }, () => {
-          sendResponse({ tabId: tab.id });
-        });
+        chrome.storage.local.set(
+          { currentItemInfo: { itemId, store, tabId: tab.id } },
+          () => {
+            sendResponse({ tabId: tab.id });
+          }
+        );
       });
     })();
     return true; // indicate async response
   } else if (message.type === 'scrapedData') {
-    const key = `scraped_${encodeURIComponent(message.itemId)}_${encodeURIComponent(message.store)}`;
+    const key = `scraped_${encodeURIComponent(message.itemId)}_${encodeURIComponent(
+      message.store
+    )}`;
     chrome.storage.local.set({ [key]: message.products });
   }
 });
